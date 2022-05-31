e3b547f0293216d008ed2d1d5e5c1599401e339a
Bikinfotech <38968856+Bikinfotech@users.noreply.github.com>
2022-05-31 01:47:48 -0500

    first commit

---
+++ src/main/java/com/myshopregistry/MyshopRegistryApplication.java
@@ -0,0 +1,16 @@
+package com.myshopregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MyshopRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyshopRegistryApplication.class, args);
+	}
+
+}
